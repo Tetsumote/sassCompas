4630d49fe2a9f6fd6748e3bd597491f05d3696f7
tetsumote <unionproject88@gmail.com>
2017-05-02 21:30:19 +0200

    com.3 End of grunt preparation

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	
 	grunt.initConfig({
 		uglify:{
@@ -9,13 +10,24 @@ module.exports = function(grunt){
 					'_/js/script.js':['_/components/js/*.js']
 				}//files
 			}//my_target
-		},//uglify	
+		},//uglify
+		compass:{
+			dev:{
+				options:{
+					config: 'config.rb'
+				}//options
+			}//dev	
+		},//compass
 		watch:{
 			options: {livereload: true },
 			scripts:{
 			files: ['_/components/js/*.js'],
 			tasks: ['uglify']
 			},//scripts	
+			sass:{
+				files: ['_/components/sass/*.scss'],
+				tasks: ['compass:dev']
+			},//sass
 			html:{
 			files: ['*.html']
 		}//html
